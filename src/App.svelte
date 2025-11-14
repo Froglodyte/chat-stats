52a4629d9c09b6833d6d2ea661db29c9adf19467
Mudit Bengani <muditbengani06@gmail.com>
2025-11-14 14:28:51 +0530

    message counter

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { countMessagesPerUser, countTotalMessages } from './lib/messageCounter';
+
+  const totalMessageCount = countTotalMessages();
+  const userMessageCounts = countMessagesPerUser();
+
+  const sortedUserMessageCounts = Object.entries(userMessageCounts).sort(([, countA], [, countB]) => countB - countA);
+</script>
+
+<main>
+  <div id = "message-count" class = "stat-card">
+    <h2>Total Messages: {totalMessageCount}</h2>
+    <ol>
+      {#each sortedUserMessageCounts as [user, count]}
+        <li><strong>{user}</strong>: <span>{count}</span></li>
+      {/each}
+    </ol>
+  </div>
+</main>
